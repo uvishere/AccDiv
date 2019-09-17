a665c5fe1c558eaf9a10094d9d6dc3375c7554c4
Yuba Raj Panta <uvishere@gmail.com>
2019-09-17 13:07:36 +0930

    common destination from given station found. API structure is set.

--- api/routes.js
+++ api/routes.js
@@ -2,13 +2,55 @@
 
 const express = require('express')
 const router = new express.Router()
+const data = require('../dataset/dataset.json')
 
-//API for from station
 
-router.get('/', async (req, res) => {
+function getStationName(id) {
+    for (const item of data) {
+        if(item.from_station_id == id) { 
+            console.log(item.from_station_name)
+            return item.from_station_name
+        } else if(item.to_station_id == id) {
+            return item.to_station_name
+        }
+    }
+}
+
+router.post('/info', async (req, res) => {
+    
+    const { station_id } = req.body
+
+    //filter rows containing requested from_station_id
+    const filtredData = data.filter(function ( {from_station_id} ) {
+        return from_station_id == station_id
+    })
+
+    const destinationCount = filtredData.reduce((agg, current) => {
+        const { to_station_id } = current
+        if (agg[to_station_id]) {
+            return {...agg, [to_station_id]: + agg[to_station_id]}
+        } else {
+            return {...agg, [to_station_id]:1}
+        }
+    }, {})
+
+    const destinations = Object.keys(destinationCount)
+
+    const commonDest = destinations.sort((a, b) => {
+        const aVal = destinationCount[a]
+        const bVal = destinationCount[b]
+        return bVal - aVal
+    })[0]
+
     res.status(200).send({
-        message: "API Initialised"
+        length: filtredData.length,
+        from_station_id: station_id,
+        from_station: getStationName(station_id),
+        common_destination_id: commonDest,
+        common_destination: getStationName(commonDest),
+        message: "done"
     })
 })
+
 
 module.exports = router
